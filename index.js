ace83d11ab9793c0b961b0c31e94e3ab0c176dac
wazed-md-abdul <wazedjiad@gmail.com>
2026-05-08 17:51:46 +0800

    understanding server side actions and process

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
 const users = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Jane Smith" },
-    { id: 3, name: "Alice Johnson" },
-    { id: 4, name: "Bob Brown" }
+    { id: 1, age:21, name: "John Doe" },
+    { id: 2, age:23, name: "Jane Smith" },
+    { id: 3, age:24, name: "Alice Johnson" },
+    { id: 4, age:25, name: "Bob Brown" }
 ]
+
+app.use(cors());
+app.use(express.json());
+app.post('/users', (req,res)=>{
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+
+    res.send({succes: true, data: req.body});
+    console.log(req.body);
+    
+})
 app.get('/', (req,res)=>{
 
     res.send("hello from express server");
